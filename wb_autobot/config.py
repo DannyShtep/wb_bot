cc6f88b45eb05191877c997ccbc8de315e95a81e
DannyShtep <113149172+DannyShtep@users.noreply.github.com>
2025-07-18 12:53:53 +0300

    Delete wb_autobot directory

--- wb_autobot/config.py
+++
@@ -1,2 +0,0 @@
-import os
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_telegram_bot_token")
--- wb_autobot/states.py
+++
@@ -1,7 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-class BookingState(StatesGroup):
-    waiting_for_token = State()
-    waiting_for_supply_choice = State()
-    waiting_for_date_range = State()
-    waiting_for_type = State()
